83c970ed2f14d86cb50a88022719c390b5b5ed1b
DDISB <disbden@gmail.com>
2023-11-06 11:36:28 +0300

    MainForm created

---
+++ simple 3d viewer project/MainForm.cpp
@@ -0,0 +1,36 @@
+#include "MainForm.h"
+
+#include <Windows.h>
+
+using namespace simple3dviewerproject;
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew MainForm);
+    return 0;
+}
+
+
+System::Void simple3dviewerproject::MainForm::MainForm_Load(System::Object^ sender, System::EventArgs^ e)
+{
+    //blackSolidThinPen = gcnew Pen(Color::Black);
+    blackSolidThinPen = gcnew Pen(Color::Tomato);
+    blackSolidThinPen->Width = 1;
+
+    bmp = gcnew Bitmap(pictureBox1->Width, pictureBox1->Height);
+    canvas = Graphics::FromImage(bmp);
+
+    
+    return System::Void();
+}
+
+System::Void simple3dviewerproject::MainForm::button1_Click(System::Object^ sender, System::EventArgs^ e)
+{    
+    canvas->Clear(Color::White);
+    canvas->DrawLine(blackSolidThinPen, 100, 100, 400, 400);
+    pictureBox1->Image = bmp;
+
+    return System::Void();
+}
+
---
+++ simple 3d viewer project/MainForm.h
@@ -0,0 +1,107 @@
+#pragma once
+
+namespace simple3dviewerproject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MainForm
+	/// </summary>
+	public ref class MainForm : public System::Windows::Forms::Form
+	{
+	public:
+		MainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::Color::White;
+			this->pictureBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->pictureBox1->Location = System::Drawing::Point(0, 0);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(1458, 647);
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(23, 32);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(88, 54);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1458, 647);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->pictureBox1);
+			this->Name = L"MainForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"MainForm";
+			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
+			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private:
+		Bitmap^ bmp;
+		Graphics^ canvas;
+		Pen^ blackSolidThinPen;
+		Color backgroundColor, color;
+
+	private: System::Void MainForm_Load(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e);
+	};
+}
